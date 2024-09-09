b24eb0c71db69a6d3b555c32f014f684cecfadb3
Yvisom Manoel <yvisom2012@gmail.com>
2024-09-09 12:32:24 -0300

    Atualizado

--- Ex-03/Ex-03/app/tela02.tsx
+++ Ex-03/Ex-03/app/tela02.tsx
@@ -1,12 +1,50 @@
-// tela02.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Image, } from 'react-native';
+import { Audio } from 'expo-av';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function Tela02() {
+  const [sound, setSound] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require('../assets/Lux Ult.mp3') 
+    );
+    setSound(sound);
+    await sound.playAsync();
+    setIsPlaying(true);
+  }
+
+  async function pauseSound() {
+    if (sound) {
+      await sound.pauseAsync();
+      setIsPlaying(false);
+    }
+  }
+
+  async function resumeSound() {
+    if (sound && !isPlaying) {
+      await sound.playAsync();
+      setIsPlaying(true);
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
+         <Image
+            source={{ uri: 'https://pbs.twimg.com/media/Fz5ZgtyaEAE8kMI.jpg'}}
+            style={styles.image}
+          />
       <Text style={styles.text}>Você está na Tela 2!</Text>
-    </View>
+       
+      <Button title="Play" onPress={playSound} />
+      <Button title="Pause" onPress={pauseSound} />
+      <Button title="Continue" onPress={resumeSound} />
+      </View>
+     
+    </ScrollView>
   );
 }
 
@@ -20,5 +58,9 @@ const styles = StyleSheet.create({
   text: {
     color: '#F7EF8A',
     fontSize: 20,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
